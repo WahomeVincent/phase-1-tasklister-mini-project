36ec1e6f1d51ab798d6e3fbb5a613b1d3f9d87b5
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-24 22:32:25 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  const submitbtn = document.getElementById("submitbtn");
+  submitbtn.addEventListener('click', function(event){
+    event.preventDefault()
+  })
+  
+  
 });
