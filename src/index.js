a3e368511f3ed18666a6dc14310aaf8d822405c3
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-25 20:09:23 +0300

    Fully functional TO-DO list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.querySelector('form').addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     console.log(e.target.new-task-description)
-    
-//   }) 
-
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
   
   let form = document.querySelector('form')
